838f6628b38a950b186d5fca23d3d28804898649
akshay-hudev <akshayhudevmanjunath@gmail.com>
2026-03-16 11:52:41 +0530

    fix: vite config env variable

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-  proxy: {
-    '/api': {
-      target: import.meta.env.VITE_API_URL || 'http://localhost:8000',
-      changeOrigin: true,
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL || 'http://localhost:8000',
+        changeOrigin: true,
+      }
     }
   }
-}
 })
