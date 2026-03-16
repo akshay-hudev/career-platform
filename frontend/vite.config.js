b07f26311fe564e5e88cc46d9c61228b415cf509
akshay-hudev <akshayhudevmanjunath@gmail.com>
2026-03-16 11:24:22 +0530

    Update fronted

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      }
+  proxy: {
+    '/api': {
+      target: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+      changeOrigin: true,
     }
   }
+}
 })
